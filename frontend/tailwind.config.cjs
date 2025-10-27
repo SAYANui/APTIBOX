b8264054e41f8bf9a6397fbdf2ab51246570b086
Ardish4 <ardishbose@gmail.com>
2025-10-27 23:03:06 +0530

    breakpoint and hero image added

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx,html}'
+  ],
+  theme: {
+    screens: {
+      // Common breakpoint sizes (adjust if you need different behavior)
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
