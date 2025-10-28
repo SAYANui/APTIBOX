3b5ea33d11041047522418f1ca6f42a16ea1f7ca
Ardish4 <ardishbose@gmail.com>
2025-10-28 12:34:56 +0530

    basic routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import { ThemeProvider } from "@/components/theme-provider"
+import { Routes, Route } from "react-router"
+
+import Home from "./pages/home"
+import Hero from "./pages/hero"
+import Login from "./pages/login"
+import Signup from "./pages/signup"
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Home />} />
+      </Routes>
     </ThemeProvider>
   )
 }
---
+++ frontend/src/pages/hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="hero-container">
+        <div className="hero-content">
+          <h1>Welcome to Aptibox</h1>
+          <p>Your one-stop solution for all your needs</p>
+          <button className="cta-button">Get Started</button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
